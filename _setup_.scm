10dcc090438c161770ef03c2e57248547f80dc2a
Ricardo G. Herdt <r.herdt@posteo.de>
2022-08-27 00:36:27 +0200

    add _setup_.scm

---
+++ _setup_.scm
@@ -0,0 +1,20 @@
+(define-module-alias json-rpc
+  (codeberg.org/rgherdt/scheme-json-rpc json-rpc))
+
+(define-module-alias (json-rpc private)
+  (codeberg.org/rgherdt/scheme-json-rpc private))
+
+(define-module-alias (json-rpc lolevel)
+  (codeberg.org/rgherdt/scheme-json-rpc lolevel))
+
+(define-module-alias (json-rpc gambit)
+  (codeberg.org/rgherdt/scheme-json-rpc gambit))
+
+(define-module-alias (chibi string)
+  (lsp-server chibi string))
+
+(define-module-alias (chibi pathname)
+  (lsp-server chibi pathname))
+
+(define-module-alias (chibi uri)
+  (lsp-server chibi uri))
