a52d39baec31baee916c03bf1c0cc571a358a2fd
Ricardo G. Herdt <r.herdt@posteo.de>
2023-04-26 23:08:26 +0200

    fix compile.sh (gambit)

--- gambit/compile.sh
+++ gambit/compile.sh
@@ -4,7 +4,7 @@ BASE_DIR=$(dirname $0)
 SOURCE_DIR=${BASE_DIR}/..
 
 echo -n "Checking for existing installation...   "
-gsi -e '(import (codeberg.org/rgherdt/scheme-lsp-server gambit util)) (exit)' >/dev/null
+gsi -e '(import (codeberg.org/rgherdt/scheme-lsp-server lsp-server private gambit)) (exit)' >/dev/null
 if [ $? -ne 0 ]; then
     echo "not found."
     echo "This scripts only compiles an existing installation of the library."
@@ -15,22 +15,20 @@ fi
 echo "found."
 echo "Building installed modules."
 
-gsc codeberg.org/rgherdt/scheme-lsp-server/gambit/util \
-    codeberg.org/rgherdt/scheme-lsp-server/private \
-    codeberg.org/rgherdt/scheme-lsp-server/parse \
-    codeberg.org/rgherdt/scheme-lsp-server/adapter \
-    codeberg.org/rgherdt/scheme-lsp-server/gambit \
-    codeberg.org/rgherdt/scheme-lsp-server/trie \
+gsc codeberg.org/rgherdt/scheme-lsp-server/lsp-server/private/gambit \
+    codeberg.org/rgherdt/scheme-lsp-server/lsp-server/private/util \
+    codeberg.org/rgherdt/scheme-lsp-server/lsp-server/private/parse \
+    codeberg.org/rgherdt/scheme-lsp-server/lsp-server/private/adapter \
+    codeberg.org/rgherdt/scheme-lsp-server/lsp-server/private/trie \
     codeberg.org/rgherdt/scheme-lsp-server/lsp-server
 
 echo "Compiling gambit-lsp-server executable."
 
 gsc -exe -nopreload ${BASE_DIR} \
-    ${SOURCE_DIR}/gambit/util.scm \
-    ${SOURCE_DIR}/private.sld \
-    ${SOURCE_DIR}/parse.sld \
-    ${SOURCE_DIR}/adapter.sld \
-    ${SOURCE_DIR}/gambit.scm \
-    ${SOURCE_DIR}/trie.sld \
+    ${SOURCE_DIR}/lsp-server/private/gambit.scm \
+    ${SOURCE_DIR}/lsp-server/private/util.sld \
+    ${SOURCE_DIR}/lsp-server/private/parse.sld \
+    ${SOURCE_DIR}/lsp-server/private/adapter.sld \
+    ${SOURCE_DIR}/lsp-server/private/trie.sld \
     ${SOURCE_DIR}/lsp-server.sld \
     ${SOURCE_DIR}/gambit/gambit-lsp-server.scm
--- gambit/gambit-lsp-server.scm
+++ gambit/gambit-lsp-server.scm
@@ -2,7 +2,7 @@
 ;; -*- Scheme -*-
 
 (import (codeberg.org/rgherdt/scheme-lsp-server lsp-server)
-        (codeberg.org/rgherdt/scheme-lsp-server private util))
+        (codeberg.org/rgherdt/scheme-lsp-server lsp-server private util))
 
 (cond-expand
  (guile (import (except (scheme base)
@@ -13,14 +13,15 @@
                 (srfi srfi-28)
                 (lsp-server private guile)))
  ((or chicken gambit)
-  (import (scheme base)
-          (scheme char) ;; string-downcase
+  (import          (scheme char) ;; string-downcase
           (scheme process-context)
           (srfi 28))))
 
 (cond-expand (chicken (import (lsp-server chicken)
+                              (scheme base)
                               (srfi 18)))
-             (gambit (import (codeberg.org/rgherdt/scheme-lsp-server lsp-server private gambit)
+             (gambit (import (gambit)
+                             (codeberg.org/rgherdt/scheme-lsp-server lsp-server private gambit)
                              (only (srfi 1) any)))
              (guile (import (only (srfi srfi-1) any)))
              (else (import (only (srfi 1) any))))
