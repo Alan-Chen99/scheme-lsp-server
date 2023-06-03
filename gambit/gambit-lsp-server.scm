dcca0713adddf0244d780c517d5193148a60e131
Ricardo G. Herdt <r.herdt@posteo.de>
2023-06-03 15:25:49 +0200

    update gambit-lsp-server.scm

--- gambit/gambit-lsp-server.scm
+++ gambit/gambit-lsp-server.scm
@@ -13,15 +13,15 @@
                 (srfi srfi-28)
                 (lsp-server private guile)))
  ((or chicken gambit)
-  (import          (scheme char) ;; string-downcase
+  (import (scheme base)
+          (scheme char) ;; string-downcase
           (scheme process-context)
           (srfi 28))))
 
-(cond-expand (chicken (import (lsp-server chicken)
-                              (scheme base)
+(cond-expand (chicken (import (lsp-server private chicken)
+                              (only (srfi 1) any)
                               (srfi 18)))
-             (gambit (import (gambit)
-                             (codeberg.org/rgherdt/scheme-lsp-server lsp-server private gambit)
+             (gambit (import (lsp-server private gambit)
                              (only (srfi 1) any)))
              (guile (import (only (srfi srfi-1) any)))
              (else (import (only (srfi 1) any))))
@@ -40,17 +40,19 @@
 
   (display (string-append "usage: "
                           program-name
-                          " [--log-level <log-level>] [--listen <port-num>] [--version]."))
+                          " [--log-level <log-level>] [--socket <port-num>] [--stdio] [--version]."))
   (newline)
-  (display (string-append "Example: " program-name " --log-level debug"))
+  (display (string-append "Example: " program-name " --log-level debug --socket 4222"))
   (newline)
   (display "Arguments: ")
   (newline)
   (display "    --log-level <level>: one of [error, warning, info, debug]. Default: error.")
   (newline)
-  (display "    --listen <port-num>: port to listen to and spawn repl upon connection.")
+  ;; (display "    --listen <port-num>: port to listen to and spawn repl upon connection.")
+  ;; (newline)
+  (display "    --stdio <port-num>: use stdio for communication with a client. If stdio and socket are ommitted, stdio is used.")
   (newline)
-  (display "    --tcp <port-num>: listen on tcp port <port-num> instead of stdio.")
+  (display "    --socket <port-num>: listen on tcp port <port-num> instead of stdio.")
   (newline)
   (display "    --help (or -h):    display this help and exit.")
   (newline)
@@ -69,7 +71,6 @@
   (any (lambda (log-level)
          (string=? log-level str))
        '("error" "warning" "info" "debug")))
-
 
 (define (process-args arguments)
   (let loop ((args arguments)
@@ -90,46 +91,51 @@
            (loop (cddr args)
                  (cons `(log-level . ,(string->symbol (cadr args)))
                        options)))
+          ((and (>= (length args) 1)
+                (string=? (car args) "--stdio"))
+           ;; default behavior. Do nothing. Added to conform with LSP
+           ;; recommendations.
+           (loop (cdr args) options))
+          ((and (>= (length args) 2)
+                (or (string=? (car args) "--tcp")
+                    (string=? (car args) "--socket"))
+                (string->number (cadr args)))
+           => (lambda (port-num)
+                (loop (cddr args)
+                      (cons `(tcp . ,port-num) options))))
           ((and (>= (length args) 2)
                 (string=? (car args) "--listen")
                 (string->number (cadr args)))
            => (lambda (port-num)
                 (loop (cddr args)
                       (cons `(listen . ,port-num) options))))
-          ((and (>= (length args) 2)
-                (string=? (car args) "--tcp")
-                (string->number (cadr args)))
-           => (lambda (port-num)
-                (loop (cddr args)
-                      (cons `(tcp . ,port-num) options))))
           (else (print-usage)
                 (exit)))))
 
-(define (main . args)
-  (let* ((options (process-args args))
-         (log-level (let ((log-level-option (assoc 'log-level options)))
+(define (main args)
+  (define options (process-args args))
+  (define log-level (let ((log-level-option (assoc 'log-level options)))
                       (if log-level-option
                           (cdr log-level-option)
                           'error)))
-         (repl-port-num
-          (let ((listen-option (assoc 'listen options)))
-            (if listen-option
-                (cdr listen-option)
-                #f)))
-         (tcp-port-num
-          (let ((tcp-option (assoc 'tcp options)))
-            (if tcp-option
-                (cdr tcp-option)
-                #f))))
+  (define repl-port-num
+    (let ((listen-option (assoc 'listen options)))
+      (if listen-option
+          (cdr listen-option)
+          #f)))
 
+  (define tcp-port-num
+    (let ((tcp-option (assoc 'tcp options)))
+      (if tcp-option
+          (cdr tcp-option)
+          #f)))
 
-    (when repl-port-num
-      (thread-start!
-       (make-thread
-        (lambda () (spawn-repl-server repl-port-num)))))
+  (when repl-port-num
+    (thread-start!
+     (make-thread
+      (lambda () (spawn-repl-server repl-port-num)))))
 
-    (parameterize ((lsp-server-log-level log-level))
-      (if tcp-port-num
-          (lsp-server-start/tcp tcp-port-num)
-          (lsp-server-start/stdio)))))
-
+  (parameterize ((lsp-server-log-level log-level))
+    (if tcp-port-num
+        (lsp-server-start/tcp tcp-port-num)
+        (lsp-server-start/stdio))))
